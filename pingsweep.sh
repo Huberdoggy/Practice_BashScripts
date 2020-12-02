f751167d882f406a4fae5bfff42de75a92f29bb5
Huberdoggy <huberdoggy@gmail.com>
2020-12-02 05:02:55 -0600

    re-expanded ping range for full subnet after testing

--- pingsweep.sh
+++ pingsweep.sh
@@ -7,7 +7,7 @@
 # Loop thru each possible host on my subnet
 echo "Now pinging with specified filters applied. Please wait..."
 printf '\n'
-for x in `seq 77 78` ; do
+for x in `seq 1 254` ; do
 # ping each 1 time. $1 is the subnet and $x is the host
 # pipe thru grep to filter for 64 byte responses
 # Then, cut the 4th segment (IP) .. '-d' is the space delimeters in this case. '-f' is field
